a98dcf097195928738317c82cf206ea9752aaab7
Shogo <hosakan888@gmail.com>
2024-08-31 17:47:04 +0900

    add: BagelShopテーブル一覧の数が多いため、ページネーションをGem kaminariで導入

--- app/controllers/bagel_shops_controller.rb
+++ app/controllers/bagel_shops_controller.rb
@@ -1,6 +1,6 @@
 class BagelShopsController < ApplicationController
   def index
-    @bagel_shops = BagelShop.all
+    @bagel_shops = BagelShop.all.page(params[:page])
     gon.bagel_shops = @bagel_shops
     gon.api_key = ENV["GMAPS_API_KEY"]
   end
