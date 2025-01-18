c392b460df90792ad2d882b76681b4993f547574
Shogo <hosakan888@gmail.com>
2025-01-18 16:00:14 +0900

    add: 検索フォームの入力に応じてフラッシュメッセージを表示＋dockerバージョンアップに伴うcompose.ymlの調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :bagel_shops, only: %i[index show]
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms_of_use', to: 'static_pages#terms_of_use'
+  get 'search', to: 'application#set_search'
 end
