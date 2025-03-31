3415b8d7e859e1d8605bab177c04bb2f9bcc3aa6
Shogo <hosakan888@gmail.com>
2025-03-31 14:57:12 +0900

    update: 絞り込み検索の入力フォームにマイナスの値が入力できないように変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("input[type='number']").forEach((input) => {
+    input.addEventListener("input", function () {
+      if (this.value < 0) this.value = 0; // 0 未満の値を入力できないようにする
+    });
+  });
+});
