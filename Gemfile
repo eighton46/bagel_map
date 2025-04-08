b6e2ebd0f9afb76c20cc16d53492527672fb4c42
Shogo <hosakan888@gmail.com>
2025-04-08 11:26:02 +0900

    add: Gem「mata-tags」を導入し静的OGPの設定

--- Gemfile
+++ Gemfile
@@ -52,13 +52,14 @@ gem "bootsnap", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-gem "dotenv-rails"
+gem 'dotenv-rails'
 gem 'google_places'
 gem 'gon'
 gem 'kaminari', '1.2.2'
 gem 'bootstrap5-kaminari-views'
 gem 'rails-i18n', '~> 7.0.0'
 gem 'ransack'
+gem 'meta-tags'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,31 @@ module ApplicationHelper
     base_title = 'Bagel Map'
     title.present? ? "#{title} | #{base_title}" : base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'BagelMap',
+      title: 'ベーグル専門店がすぐ見つかる地図検索アプリ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'BagelMapを使えば、美味しいベーグル専門店を地図上から簡単に検索できます。現在地から探す機能も搭載！',
+      keywords: 'ベーグル,ベーグル専門店,パン屋,地図検索,現在地検索,東京,グルメ',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp_bagel.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        # site: '@', # Twitterアカウントがあれば記載
+        image: image_url('ogp_bagel.png')
+      }
+    }
+  end
 end
